4a8ee3788e220700d78cc6bba48ff1094e50e600
David Carrington <dmcarrington@protonmail.com>
2023-05-02 15:13:16 +0100

    tidied, added postman collection

---
+++ interfaces/lnbits.ts
@@ -0,0 +1,8 @@
+export interface LnbitsAccount {
+  lnurlKey: string;
+  email: string;
+  lnbitsUsername: string;
+  lnbitsUserId: string;
+  lnbitsWalletId: string;
+  lnbitsWalletName: string;
+}
